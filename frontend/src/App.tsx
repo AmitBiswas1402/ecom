7f157895851691bda958f66bb3e3861534206026
Amit Biswas <amit.142biswas@gmail.com>
2024-07-18 22:18:03 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {lazy} from 'react'
+
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
+
+const App = () => {
+  return (
+    <Router>
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/cart' element={<Cart />} />
+
+      </Routes>
+
+    </Router>
+  )
+}
+
+export default App
